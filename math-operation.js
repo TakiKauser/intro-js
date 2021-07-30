c520308e6325ac95b148eee6b8934087a552797a
TakiKauser <takikauserbadhon@gmail.com>
2021-07-31 04:41:28 +0600

    operation short hand

---
+++ math-operation.js
@@ -0,0 +1,33 @@
+// recap
+
+var price1 = 21;
+var price2 = 19;
+var sum = price1 + price2;
+var difference = price2 - price1;
+var multiplication = price1 * price2;
+var division = price1 / price2;
+
+// console.log(sum);
+// console.log(difference);
+// console.log(multiplication);
+// console.log(division);
+
+var tripledPrice = price2 * 3;
+// var newPrice = tripledPrice + 10;
+// console.log(newPrice);
+
+// tripledPrice = tripledPrice + 10;
+tripledPrice += 10;
+console.log(tripledPrice);
+
+var age = 26;
+age = age + 1;
+age += 1;
+age++;
+console.log(age);
+
+var love = 100;
+love = love - 1;
+love -= 1;
+love--;
+console.log(love);
