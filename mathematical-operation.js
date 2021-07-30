5cacc77d524ebbf884fcfd1aecb1e752ea5fa34f
TakiKauser <takikauserbadhon@gmail.com>
2021-07-31 04:21:29 +0600

    operation in js

---
+++ mathematical-operation.js
@@ -0,0 +1,23 @@
+var onionPrice = 50;
+var eggPrice = 32;
+var eggQuantity = 3;
+
+var moneyAvailable = 100;
+var OrangePrice = 20;
+
+var totalPrice = onionPrice + eggPrice;
+var priceDifference = onionPrice - eggPrice;
+var eggPriceMultification = eggPrice * eggQuantity; 
+var orangeQuantity = moneyAvailable / OrangePrice;
+
+// console.log(onionPrice);
+// console.log(eggPrice);
+// console.log(onionPrice+eggPrice);
+
+console.log(totalPrice);
+console.log(priceDifference);
+console.log(eggPriceMultification);
+console.log(orangeQuantity);
+
+
+
