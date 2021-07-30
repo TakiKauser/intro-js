cb6992d8784aac3ca080b8bec2f022fb524e8cab
TakiKauser <takikauserbadhon@gmail.com>
2021-07-31 04:04:40 +0600

    var types & names

---
+++ var-name.js
@@ -0,0 +1,21 @@
+var name = "Dany Alan";
+
+// reserved keywords not allowed
+var false = 0;
+
+// neme in one words(without_space)
+var name age = 39;
+
+// quote not allowed
+var "address" = "Dhaka";
+
+// 
+var my-office-address = "Chittagong";
+var my_office_address = "Chittagong"; 
+var myOfficeAddress = "Chittagong";
+var myofficeaddress = "Chittagong";
+var PIE = 3.14;
+
+// 
+var 69site = 3;
+var h2o = 11;
---
+++ variable.js
@@ -0,0 +1,12 @@
+// numeric
+var price = 99;
+var age = 26;
+var temparature = 32;
+var sallery = 0;
+
+// string
+var name = "Ali";
+
+// boolean
+var password = true;
+var subscribed = false;
