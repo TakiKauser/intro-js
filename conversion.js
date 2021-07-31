5b694e75dda5b17e5c2705d5f4b39c8aa0916ed7
TakiKauser <takikauserbadhon@gmail.com>
2021-08-01 00:15:22 +0600

    type conversion

---
+++ conversion.js
@@ -0,0 +1,23 @@
+var firstName = "Taki";
+var lastName = "Kauser";
+var fullName = firstName + " " + lastName;
+// console.log(fullName);
+
+var onionPrice = "42.44";
+var eggPrice = "41.55";
+var onionPriceNumber = parseFloat(onionPrice);
+var eggPriceNumber = parseFloat(eggPrice);
+// console.log(parseInt(totalPrice));
+// var totalPrice = onionPrice + eggPrice;
+var totalPriceNumber = onionPriceNumber + eggPriceNumber;
+// console.log(totalPrice);
+console.log(parseInt(totalPriceNumber));
+console.log(parseFloat(totalPriceNumber));
+
+
+
+// integer
+var sunglass = 499;
+
+// float
+var shoe = 4999.99;
